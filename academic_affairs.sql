b180282e036f241c0137c30072c5b84df1fa527a
Baker Renneckar <bakerrenneckar@gmail.com>
2021-05-24 12:10:46 -0700

    adjusted 12th grade, official readiness

--- academic_affairs.sql
+++ academic_affairs.sql
@@ -11,8 +11,14 @@ WITH gather_data AS (
     -- % of seniors who are composite ready
     -- Will need to add in the opt-out indicator once it is made.
     CASE
-      WHEN composite_readiness_most_recent_c = '1. Ready'
-      AND grade_c = '12th Grade' THEN 1
+      WHEN Readiness_Composite_Off_c = '1. Ready'
+      AND (
+        grade_c = '12th Grade'
+        OR (
+          grade_c = 'Year 1'
+          AND years_since_hs_grad_c = 0
+        )
+      ) THEN 1
       ELSE 0
     END AS composite_ready
   FROM
@@ -24,30 +30,31 @@ WITH gather_data AS (
 -- % of students with 80% attendance in AA workshops
 -- This KPI is done over two CTEs the filtering occurs in the first and the logic occurs in the second
 aa_attendance_prep AS (
-    SELECT
-      C.student_c,
+  SELECT
+    C.student_c,
     CASE
       WHEN SUM(Attendance_Denominator_c) = 0 THEN NULL
       ELSE SUM(Attendance_Numerator_c) / SUM(Attendance_Denominator_c)
     END AS attendance_rate
-    FROM
-      `data-warehouse-289815.salesforce_clean.class_template` C
-      LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_at_template` CAT ON CAT.global_academic_semester_c = C.global_academic_semester_c
-    WHERE
-      department_c = "Academic Affairs"
-      AND CAT.AY_Name = 'AY 2020-21'
-      
-    GROUP BY student_c
-  ),
-  
-aa_attendance_kpi AS (
-SELECT student_c,
-CASE WHEN attendance_rate >= 0.8 THEN 1
-ELSE 0
-END AS above_80_aa_attendance
-FROM aa_attendance_prep
+  FROM
+    `data-warehouse-289815.salesforce_clean.class_template` C
+    LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_at_template` CAT ON CAT.global_academic_semester_c = C.global_academic_semester_c
+  WHERE
+    department_c = "Academic Affairs"
+    AND CAT.AY_Name = 'AY 2020-21'
+  GROUP BY
+    student_c
 ),
-
+aa_attendance_kpi AS (
+  SELECT
+    student_c,
+    CASE
+      WHEN attendance_rate >= 0.8 THEN 1
+      ELSE 0
+    END AS above_80_aa_attendance
+  FROM
+    aa_attendance_prep
+),
 gather_survey_data AS (
   SELECT
     CT.site_short,
@@ -60,26 +67,24 @@ gather_survey_data AS (
     END AS i_am_in_control_of_my_academic_performance,
     -- Growth in the % of students that agree or strongly agree to this statement: “I feel prepared to engage in academic stretch opportunities.” 
     -- Not doing any growth calculation here, just displaying the raw percent. Can follow up with VS if that is a concern. 
-        CASE
+    CASE
       WHEN i_feel_prepared_to_engage_in_academic_stretch_opportunities = "Strongly Agree" THEN 1
       WHEN i_feel_prepared_to_engage_in_academic_stretch_opportunities = 'Agree' THEN 1
       ELSE 0
     END AS i_feel_prepared_to_engage_in_academic_stretch_opportunities
-    
   FROM
     `data-studio-260217.surveys.fy21_hs_survey` S
     LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_template` CT ON CT.Contact_Id = S.contact_id
- 
 )
-
-  
 SELECT
   GD.site_short,
   SUM(above_325_gpa) AS aa_above_325_gpa,
   SUM(composite_ready) AS aa_composite_ready,
   SUM(above_80_aa_attendance) AS aa_above_80_aa_attendance,
   SUM(i_am_in_control_of_my_academic_performance) AS aa_i_am_in_control_of_my_academic_performance,
-SUM(i_feel_prepared_to_engage_in_academic_stretch_opportunities) AS aa_i_feel_prepared_to_engage_in_academic_stretch_opportunities
+  SUM(
+    i_feel_prepared_to_engage_in_academic_stretch_opportunities
+  ) AS aa_i_feel_prepared_to_engage_in_academic_stretch_opportunities
 FROM
   gather_data GD
   LEFT JOIN aa_attendance_kpi AA ON GD.Contact_Id = AA.student_c
