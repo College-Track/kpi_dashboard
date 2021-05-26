74d38e85353074d75bd0ec0fef7dcdf77f0f089d
Baker Renneckar <bakerrenneckar@gmail.com>
2021-05-26 10:00:48 -0700

    IR's KPIs take 2

--- wellness.sql
+++ wellness.sql
@@ -1,289 +1,181 @@
-WITH --gather contact and academic term data to join with COVI data
+WITH --Gather contact and academic term data to join with COVI data to set reporting groups
 gather_at_data AS (
     SELECT
         full_name_c,
         at_id,
-        at_name,
         contact_id,
         AY_Name,
-        site
+        site_short
     FROM
         `data-warehouse-289815.salesforce_clean.contact_at_template`
     WHERE
         record_type_id = '01246000000RNnSAAW' --HS student contact record type
         AND site != 'College Track Arlen'
         AND College_Track_Status_Name = 'Current CT HS Student'
-        AND AY_Name = 'AY 2020-21' --To pull in Covi test records within this given AY 
 ),
+--Pull Covi assessments completed within appropriate AYs (2019-20, 2020-21)
 gather_covi_data AS (
     SELECT
         contact_name_c AS contact_id_covi,
-        academic_semester_c AS covi_at,
-        co_vitality_scorecard_color_c,
-        belief_in_self_raw_score_c,
-        belief_in_others_raw_score_c,
-        emotional_competence_raw_score_c,
-        engaged_living_raw_score_c,
-        --add Covi Domain scores to obtain total raw Covitality score
-        SUM(
-            belief_in_self_raw_score_c + belief_in_others_raw_score_c + emotional_competence_raw_score_c + engaged_living_raw_score_c
-        ) AS raw_covi_score,
-        version_c,
-        status_c,
-        --test_date_c, 
-        co_vitality_test_completed_date_c,
+        contact_id,
+        --from contact_at
         id AS test_record_id,
-        student_site_c,
-        record_type_id
+        site_short,
+        AY_Name,
+        belief_in_self_raw_score_c,
+        engaged_living_raw_score_c,
+        belief_in_others_raw_score_c,
+        emotional_competence_raw_score_c
     FROM
         `data-warehouse-289815.salesforce_clean.test_clean` AS COVI
+        LEFT JOIN gather_at_data AS GAD ON GAD.at_id = COVI.academic_semester_c
     WHERE
-        record_type_id = '0121M000001cmuDQAQ' --Covitality test record type
+        COVI.record_type_id = '0121M000001cmuDQAQ' --Covitality test record type
         AND status_c = 'Completed'
-    GROUP BY
-        contact_name_c,
-        academic_semester_c,
-        co_vitality_scorecard_color_c,
-        belief_in_self_raw_score_c,
-        belief_in_others_raw_score_c,
-        emotional_competence_raw_score_c,
-        engaged_living_raw_score_c,
-        version_c,
-        status_c,
-        co_vitality_test_completed_date_c,
-        id,
-        --test record id
-        student_site_c,
-        record_type_id
-) --Join contact_at data with COVI data to obtain contact id and pull in 2020-21AY Covi data
---join_term_data_with_covi AS (
-SELECT
-    full_name_c,
-    contact_id_covi,
-    co_vitality_test_completed_date_c,
-    at_id,
-    covi_at,
-    --at_name,
-    student_site_c,
-    raw_covi_score,
-    contact_id,
-    AY_NAME,
-    test_record_id,
-    CASE
-        WHEN test_record_id IS NOT NULL THEN 1
-        ELSE 0
-    END AS covi_assessment_completed_ay
-FROM
-    gather_at_data AS A --LEFT JOIN gather_covi_data AS C ON A.contact_id = C.contact_id_covi
-    LEFT JOIN gather_covi_data AS C ON A.at_id = C.covi_at
-GROUP BY
-    full_name_c,
-    contact_id_covi,
-    co_vitality_test_completed_date_c,
-    at_id,
-    covi_at,
-    --at_name,
-    student_site_c,
-    raw_covi_score,
-    contact_id,
-    AY_NAME,
-    test_record_id
+        AND AY_Name IN ('AY 2019-20', 'AY 2020-21')
 ),
-gather_students_with_more_than_1_covi AS (
+--Setting groundwork for KPI indicator: % of students who have taken the the CoVi assessment each academic year (2020-21AY)
+--Data gathered through 2 CTEs. Denom should be Current CT HS Student
+--Wondering if this should be 2019-20AY instead?
+completing_covi_data AS (
     SELECT
+        site_short,
         contact_id,
-        SUM(covi_assessment_completed_ay) AS sum_of_covi_tests_taken_ay #does sudent have more than 1 covi assessment?
+        CASE
+            WHEN test_record_id IS NOT NULL THEN 1
+            ELSE 0
+        END AS covi_assessment_completed_ay
     FROM
-        join_term_data_with_covi
+        gather_covi_data
+    WHERE
+        AY_Name = 'AY 2020-21'
+    GROUP BY
+        contact_id,
+        site_short,
+        test_record_id
+),
+--Isolate students that completed a Covitality assessment in 2020-21AY
+students_that_completed_covi AS (
+    SELECT
+        COUNT(DISTINCT contact_id) AS wellness_covi_assessment_completed_ay,
+        site_short
+    FROM
+        completing_covi_data
     WHERE
         covi_assessment_completed_ay = 1
     GROUP BY
-        contact_id
+        site_short
 ),
-gather_first_covi_test_date AS (
+--Using same logic from Site Director KPIs to calculate: % of students growing toward average or above social-emotional strengths
+--This KPI is done over four CTEs. The majority of the logic is done in the second CTE.
+data_for_social_emotional_growth AS (
     SELECT
-        (
-            SELECT
-                MIN(co_vitality_test_completed_date_c)
-            FROM
-                join_term_data_with_covi j2
-            WHERE
-                j.contact_id = j2.contact_id
-        ) AS first_covi_ay,
-        j.contact_id,
-        co_vitality_test_completed_date_c,
-        raw_covi_score,
-        raw_covi_score AS first_covi_score,
-        student_site_c
+        contact_id_covi,
+        site_short,
+        AY_Name,
+        --add Covi Domain scores to obtain total raw Covitality score. Use lowest score if student has 1+ Covi
+        MIN(
+            belief_in_self_raw_score_c + engaged_living_raw_score_c + belief_in_others_raw_score_c + emotional_competence_raw_score_c
+        ) AS covi_raw_score
     FROM
-        gather_students_with_more_than_1_covi AS c
-        LEFT JOIN join_term_data_with_covi AS j ON c.contact_id = j.contact_id
+        gather_covi_data
     WHERE
-        AY_Name = 'AY 2020-21'
-        AND sum_of_covi_tests_taken_ay > 1 #only students with 2 or more tests to assess growth
+        AY_Name IN ('AY 2019-20', 'AY 2020-21')
     GROUP BY
-        contact_id,
-        co_vitality_test_completed_date_c,
-        raw_covi_score,
-        student_site_c
+        site_short,
+        contact_id_covi,
+        AY_Name
+    ORDER BY
+        site_short,
+        contact_id_covi,
+        AY_Name
 ),
-gather_last_covi_test_date AS (
+calc_covi_growth AS (
     SELECT
-        (
-            SELECT
-                MAX(co_vitality_test_completed_date_c)
-            FROM
-                join_term_data_with_covi j2
-            WHERE
-                j.contact_id = j2.contact_id
-        ) AS last_covi_ay,
-        j.contact_id,
-        co_vitality_test_completed_date_c,
-        student_site_c,
-        raw_covi_score AS last_covi_score,
-        raw_covi_score
+        site_short,
+        contact_id_covi,
+        covi_raw_score - lag(covi_raw_score) over (
+            partition by contact_id_covi
+            order by
+                AY_Name
+        ) AS covi_growth
     FROM
-        gather_students_with_more_than_1_covi AS c
-        LEFT JOIN join_term_data_with_covi AS j ON c.contact_id = j.contact_id
-    WHERE
-        AY_Name = 'AY 2019-20'
-        AND sum_of_covi_tests_taken_ay > 1 #only students with 2 or more tests to assess growth
-    GROUP BY
-        contact_id,
-        co_vitality_test_completed_date_c,
-        raw_covi_score,
-        student_site_c
+        data_for_social_emotional_growth
 ),
-covi_score_first_test_ay AS (
+covi_growth_indicator AS (
     SELECT
-        contact_id,
-        student_site_c,
-        PERCENTILE_CONT(first_covi_score,.5) OVER (PARTITION by student_site_c) AS prep_first_raw_covi_score_median_ay,
-        #median
-        first_covi_ay
-    FROM
-        gather_first_covi_test_date AS A
-    WHERE
-        co_vitality_test_completed_date_c = first_covi_ay #filter for first test date
-        AND first_covi_score = (
-            select
-                MIN(A2.first_covi_score)
-            FROM
-                gather_first_covi_test_date AS A2
-            where
-                A.contact_id = A2.contact_id
-        )
-        AND raw_covi_score = first_covi_score --pull lowest CoVi score if student has more than 1 test on the same date
-    GROUP BY
-        contact_id,
-        first_covi_score,
-        first_covi_ay,
-        student_site_c
-),
-covi_score_last_test_ay AS (
-    SELECT
-        contact_id,
-        student_site_c,
-        PERCENTILE_CONT(last_covi_score,.5) OVER (PARTITION by student_site_c) AS prep_last_raw_covi_score_median_ay,
-        #median
-        last_covi_ay
-    FROM
-        gather_last_covi_test_date AS A
-    WHERE
-        co_vitality_test_completed_date_c = last_covi_ay #filter for last test date
-        AND last_covi_score = (
-            select
-                MIN(A2.last_covi_score)
-            FROM
-                gather_last_covi_test_date AS A2
-            where
-                A.contact_id = A2.contact_id
-        )
-        AND raw_covi_score = last_covi_score --pull lowest CoVi score if student has more than 1 test on the same date
-    GROUP BY
-        contact_id,
-        last_covi_score,
-        last_covi_ay,
-        student_site_c
-),
-/*
- gather_casenotes_data AS (
- SELECT 
- )
- */
-prep_median_growth_kpi AS (
-    SELECT
-        j.student_site_c,
-        MAX(prep_first_raw_covi_score_median_ay) AS first_raw_covi_score_median_ay,
-        MAX(prep_last_raw_covi_score_median_ay) AS last_raw_covi_score_median_ay
-    FROM
-        join_term_data_with_covi AS j
-        LEFT JOIN covi_score_first_test_ay AS A ON j.student_site_c = A.student_site_c
-        LEFT JOIN covi_score_last_test_ay AS B ON j.student_site_c = B.student_site_c
-    GROUP BY
-        j.student_site_c
-),
-prep_kpi AS (
-    SELECT
-        A.student_site_c,
-        first_raw_covi_score_median_ay,
-        last_raw_covi_score_median_ay,
-        SUM(covi_assessment_completed_ay) AS wellness_students_completing_covi_ay,
+        site_short,
+        contact_id_covi,
+        --Indicator for students demonstrating growth in Covi taken between 2019-20 and 2020-21
         CASE
-            WHEN last_raw_covi_score_median_ay > first_raw_covi_score_median_ay THEN 1
+            WHEN covi_growth > 0 THEN 1
             ELSE 0
-        END AS wellness_covi_median_growth
+        END AS covi_student_grew
     FROM
-        join_term_data_with_covi as A --LEFT JOIN gather_covi_data as C ON C.academic_semester_c = A.at_id
-        LEFT JOIN covi_score_first_test_ay AS CF ON CF.contact_id = A.contact_id
-        LEFT JOIN covi_score_last_test_ay AS CL ON CL.contact_id = A.contact_id
-        LEFT JOIN prep_median_growth_kpi AS M ON M.student_site_c = A.student_site_c
+        calc_covi_growth
+    WHERE
+        covi_growth IS NOT NULL
+),
+aggregate_growth_covi_data AS (
+    SELECT
+        site_short,
+        SUM(covi_student_grew) AS wellness_covi_student_grew,
+        COUNT(DISTINCT contact_id_covi) AS wellness_covi_growth_denominator
+    FROM
+        covi_growth_indicator
     GROUP BY
-        student_site_c,
-        first_raw_covi_score_median_ay,
-        last_raw_covi_score_median_ay,
-        last_covi_ay,
-        first_covi_ay
+        site_short
+),
+-- % of students served by Wellness who "strongly agree" wellness services assisted them in managing their stress, helping them engage in self-care practices and/or enhancing their mental health
+-- The denominator for this metric will be students who answered "yes" to receiving wellness services
+gather_wellness_survey_data AS (
+    SELECT
+        CT.site_short,
+        S.contact_id AS students_receiving_wellness_services,
+        CASE
+            WHEN (
+                working_with_college_track_wellness_services_has_assisted_you_in_managing_your_s IN ("Strongly Agree", "Totalmente de acuerdo")
+                OR working_with_college_tracks_wellness_programming_has_helped_you_engage_in_self_c IN ("Strongly Agree", "Totalmente de acuerdo")
+                OR working_with_college_tracks_wellness_services_has_enhanced_your_mental_health IN ("Strongly Agree", "Totalmente de acuerdo")
+            ) THEN 1
+            ELSE 0
+        END AS strongly_agree_wellness_services_assisted_them
+    FROM
+        `data-studio-260217.surveys.fy21_hs_survey` S
+        LEFT JOIN `data-warehouse-289815.salesforce_clean.contact_template` CT ON CT.Contact_Id = S.contact_id
+    WHERE
+        did_you_engage_with_wellness_services_at_your_site = "Yes"
+),
+aggregate_wellness_survey_data AS (
+    SELECT
+        COUNT (DISTINCT students_receiving_wellness_services) AS wellness_survey_wellness_services_assisted_denom,
+        SUM(strongly_agree_wellness_services_assisted_them) AS wellness_survey_wellness_services_assisted_num,
+        site_short
+    FROM
+        gather_wellness_survey_data
+    GROUP BY
+        site_short
+),
+--Growth KPIs, survey KPI, students completing Covitality KPI
+prep_all_wellness_kpis AS (
+    SELECT
+        A.site_short,
+        wellness_covi_assessment_completed_ay,
+        wellness_covi_student_grew,
+        wellness_covi_growth_denominator,
+        wellness_survey_wellness_services_assisted_num,
+        wellness_survey_wellness_services_assisted_denom
+    FROM
+        aggregate_growth_covi_data AS A
+        LEFT JOIN students_that_completed_covi AS B ON A.site_short = B.site_short
+        LEFT JOIN aggregate_wellness_survey_data AS C ON A.site_short = C.site_short
 )
 SELECT
-    wellness_students_completing_covi_ay,
-    wellness_covi_median_growth,
-    first_raw_covi_score_median_ay,
-    last_raw_covi_score_median_ay,
-    student_site_c
+    site_short,
+    wellness_covi_assessment_completed_ay,
+    wellness_covi_student_grew,
+    wellness_covi_growth_denominator,
+    wellness_survey_wellness_services_assisted_num,
+    wellness_survey_wellness_services_assisted_denom
 FROM
-    prep_kpi
-GROUP BY
-    wellness_students_completing_covi_ay,
-    student_site_c,
-    wellness_covi_median_growth,
-    first_raw_covi_score_median_ay,
-    last_raw_covi_score_median_ay
-    /*
-     --Commenting out subqueries - too much processing    
-     gather_first_and_last_covi_test_date AS (
-     SELECT 
-     co_vitality_test_completed_date_c,
-     raw_covi_score, 
-     student_site_c,
-     j.contact_id,
-     (SELECT MIN(co_vitality_test_completed_date_c)
-     FROM join_term_data_with_covi j2 
-     WHERE j.contact_id = j2.contact_id
-     ) AS first_covi_ay,
-     
-     (SELECT MAX(co_vitality_test_completed_date_c)
-     FROM join_term_data_with_covi j2 
-     WHERE j.contact_id = j2.contact_id
-     ) AS last_covi_ay
-     
-     FROM gather_students_with_more_than_1_covi AS c
-     LEFT JOIN join_term_data_with_covi AS j ON c.contact_id = j.contact_id
-     WHERE AY_Name = 'AY 2019-20'
-     AND sum_of_covi_tests_taken_ay > 1 #only students with 2 or more tests to assess growth
-     GROUP BY
-     student_site_c,
-     raw_covi_score, 
-     j.contact_id,
-     co_vitality_test_completed_date_c
-     ),*/
+    prep_all_wellness_kpis
